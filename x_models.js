bbb7e3969065ca5e43d5444f865767d30bdf653a
williamle8300 <williamle8300@gmail.com>
2014-03-26 11:22:27 -0700

    asdf

--- x_models.js
+++ x_models.js
@@ -22,9 +22,12 @@ _track: {
     {
       resourceID: '384859',
       encodedObjHTML: '#%##%##%##%##%##%##%##%##%#',
-      ?tracksetMemberOf: [324, 2341901, 4451, 85810],
-      ?provider: 'soundcloud_com',
-      ?playCount: 3949
+      tracksetMemberOf: [324, 2341901, 4451, 85810],
+    },
+    {
+      resourceID: '221',
+      encodedObjHTML: '#%##%##%##%##%##%##%##%##%#',
+      tracksetMemberOf: [],
     },
     •••
   ]
@@ -34,23 +37,24 @@ _track: {
 //===========TRACKSETS=======//
 //===========================//
 
-_trackSet.list[2].setID // 2
-_trackSet.list[2].setList // ["948548", "7785422", "949331", "410200", "1103948", "991222"]
+_trackSet.list[3].setID // 4451
+_trackSet.list[3].setList // [948548, 7785422, 949331, 410200, 1103948, 991222]
 
 _trackset: {
 	list: [
 		{
+	   name: 'LABELED',//everytime anything is done with a trackSet (e.g., saves, removes)
+	   setID: 0,
+	   setList: [948548, 7785422, 949331, 410200, 1103948, 991222, 948548, 7785422, 949331, 410200, 1103948, 991222, 948548, 7785422, 949331, 410200, 1103948, 991222],
+	  },
+		•••
+		{
 	   name: 'Canadian Indie',
 	   setID: 4451,
-	   setList: ["948548", "7785422", "949331", "410200", "1103948", "991222"]
-	   ?signals: [ {userID: 'ObjectId("52f45b6c269516000007")', setID: '32'}, {userID: 'ObjectId("12x195b6c2695160000d7")', setID: '21'}, {userID: 'ObjectId("22x19512x192695160011d7")', setID: '33'}, {userID: 'ObjectId("3442x19512x19269517")', setID: '17'}, {userID: 'ObjectId("342a2f4512x19269516057")', setID: '4'} ],
-	   ?playCount: 2093
-	   ?signedAt: {userID: 'ObjectId("52f45b6c26951600000511d7")', setID: '32', urlForm: 'mawtrombone?trackset=pete-seeger-classics'},
+	   setList: [948548, 7785422, 949331, 410200, 1103948, 991222]
 	  },
-  •••
-	],
-	?lastPlayed: [ ••• ],
-	?lastModified: [ ••• ]
+	  •••
+	]
 }
 
 //===========================//
