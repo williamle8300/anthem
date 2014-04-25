09b28f32b28432fe920289a8f3835622b7b6d471
williamle8300 <williamle8300@gmail.com>
2014-04-24 19:18:19 -0700

    adsf

--- controllers/profile.js
+++ controllers/profile.js
@@ -10,38 +10,50 @@ var _ = require('lodash');
 
 exports.getProfile = function (req, res, next) {
   var username = req.params.username;
-  User.findOne({username: username}, function(err, profileUser) {
-    if (!profileUser) return next(err);
-    var profileUser = {
-      username: profileUser.username,
-      _track: profileUser._track,
-      _trackSet: profileUser._trackSet,
-      //profile: profileUser.profile
+  User.findOne({username: username}, function(err, userObj) {
+    if (!userObj) return next(err);
+    var userObj = {
+      username: userObj.username,
+      _track: userObj._track,
+      _trackSet: userObj._trackSet,
+      //profile: userObj.profile
     };
     res.render('profile/profileIndex.html', {
       app: 'Anthem',
       title: 'Songs',
+      userObj: userObj,
       success: req.flash('success'),
-      error: req.flash('error'),
-      profileUser: profileUser
+      error: req.flash('error')
     });
   });
 };
 
 exports.getTrackSet = function (req, res, next) {
 	var username = req.params.username;
-	var trackSetName = req.params.trackSet;
+	var trackSet = req.params.trackSet;
 
 	User.findOne({username: username}, function(err, userObj){
-		var userTrackSetList = userObj.musicCollection.trackSets.list
+		var userTrackSetList = userObj.musicCollection.trackSets.list;
+		var userObj = userObj;
+		var setList = [];
 
 		if(!userObj) {//username doesn't exists... just end the request
 			console.log('> accidental GET request for a trackSet! ' +req.url);
 			res.end();
 		} else {
-			console.log(_.find(userTrackSetList, {'name': trackSetName}));
-			//RENDER with the trackSet array
-			res.end();
+			setList = _.find(userTrackSetList, {'name': trackSet}).setList;
+			console.log(setList);
+	    userObj = {
+	      username: userObj.username,
+				setList: setList
+	    };
+	    res.render('profile/profileTrackSet.html', {
+	      app: 'Anthem',
+	      title: trackSet,
+	      userObj: userObj,
+	      success: req.flash('success'),
+	      error: req.flash('error')
+	    });
 		}
 	});
 }
